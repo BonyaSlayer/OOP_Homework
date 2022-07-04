3db24fcd1d764a62784ad175b70990c804233f12
BonyaSlayer <zlobasterus@gmail.com>
2022-07-04 19:06:39 +0500

    ДЗ часть 2

---
+++ OOP_Homework_2/BankCount.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Homework_2
+{
+    public class BankCount
+    {
+       private   int accountNumber;
+
+       private static int count;
+
+       private int balance;
+
+       private counttype typeofcount;
+       
+
+       public enum counttype
+        {
+            rub,
+            dollar,
+            euro,
+        }
+        /// <summary>
+        /// Метод генерации номера счета
+        /// </summary>
+        public void GenerateAccountNumber()
+        {
+            count = count+1;
+            this.accountNumber = count;
+
+        }      
+        
+        /// <summary>
+        /// Метод заполнения полей счета
+        /// </summary>
+        /// <param name="value1">Номер счета</param>
+        /// <param name="value2">Баланс</param>
+        /// <param name="value3">Тип 0-2</param>
+        /// <returns></returns>
+        public void SetCount(int balance, int type)
+        {
+            //this.count = value1;
+            count++;
+            this.accountNumber = count;
+            this.balance = balance;
+            this.typeofcount = (counttype)type;
+            
+            
+        }
+        
+
+        public void PrintCount()
+        {
+            Console.WriteLine($" Ваш тип счета: {typeofcount}\n Ваш номер счета: {accountNumber}\n Ваш баланс: {balance}\n ");
+
+        }
+        public int AccountNumber { get { return accountNumber; } set { accountNumber = value; } }
+        public int Balance { get { return balance; } set { balance = value; } }
+        public counttype TypeofCount { get { return typeofcount; } set { typeofcount = value; } }
+
+
+    }
+}
--- OOP_Homework_2/Program.cs
+++ OOP_Homework_2/Program.cs
@@ -1,2 +1,18 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using OOP_Homework_2;
+
+
+BankCount mycount = new BankCount();
+BankCount mycount2 = new BankCount();
+BankCount mycount3 = new BankCount();
+
+
+mycount.GenerateAccountNumber();
+mycount.PrintCount();
+
+mycount2.GenerateAccountNumber();
+mycount2.PrintCount();
+
+mycount3.GenerateAccountNumber();
+mycount3.PrintCount();
+
+Console.ReadKey(true);

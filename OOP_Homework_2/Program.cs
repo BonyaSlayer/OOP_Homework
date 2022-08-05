70e2176298fed7667b4377120a7feeeb65f715eb
BonyaSlayer <104202807+BonyaSlayer@users.noreply.github.com>
2022-08-06 00:52:21 +0500

    Update Program.cs

--- OOP_Homework_2/Program.cs
+++ OOP_Homework_2/Program.cs
@@ -6,7 +6,7 @@ Building building1 = new Building(14, 5, 45, 3);
 Building building2 = new Building(14, 5, 80, 4);
 
 
-Console.WriteLine($"Высота одного подъезда в доме {building1.Id} = {Building.GetFloorHeight(building1)} м.");
+Console.WriteLine($"Высота одного этажа в доме {building1.Id} = {Building.GetFloorHeight(building1)} м.");
 Console.WriteLine(Building.GetEntarnceRooms(building1));
 Console.WriteLine(Building.GetFloorRooms(building1));
 
